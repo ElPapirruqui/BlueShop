2826d2b276ea0fb562cf420b5665c67571a74271
Ignacio Urrutia <ignaciourrutiamichelini@gmail.com>
2023-12-09 21:18:42 -0300

    Refactored player movement to use Rigidbody instead of transform position to prevent collisions glitches

--- Assets/BlueShop/Scripts/Player/PlayerMovement.cs
+++ Assets/BlueShop/Scripts/Player/PlayerMovement.cs
@@ -5,11 +5,12 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] private float moveSpeed = 1.0f;
+    [SerializeField] private Rigidbody2D rigidBody;
 
     //Move the transform using the new direction
     public void Move(Vector2 newDirection)
     {
-        Vector3 moveDirection = new Vector3(newDirection.x, newDirection.y);
-        transform.position += moveDirection * moveSpeed * Time.deltaTime;
+        Vector2 moveDirection = newDirection * moveSpeed;
+        rigidBody.velocity = moveDirection;
     }
 }
